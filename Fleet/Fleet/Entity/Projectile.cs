73d8aed29b6a86c5422377364038031deb0c8db0
Stephen Eisen <stephen.eisen27@gmail.com>
2019-01-07 05:42:23 -0500

    Added simple timecheck for projectiles

--- Fleet/Fleet/Entity/Projectile.cs
+++ Fleet/Fleet/Entity/Projectile.cs
@@ -11,6 +11,8 @@ namespace Fleet.Entity
 		private Vector2 _targetLocation;
 		private Vector2 _startPosition;
 
+    private float timer;
+
 		public Projectile(string filePath) : base(filePath) { }
 
 		public Projectile(string filePath, Vector2 playerPosition) : base(filePath)
@@ -25,10 +27,11 @@ namespace Fleet.Entity
     {
       var mouseState = Mouse.GetState();
 
-      this.position = playerPosition;
-      _targetLocation = GameManager.Instance.camera.ScreenToWorld(new Vector2(mouseState.X, mouseState.Y));
+      timer = 20f;
 
+      this.position = playerPosition;
       this.rotation = playerRotation;
+      _targetLocation = GameManager.Instance.camera.ScreenToWorld(new Vector2(mouseState.X, mouseState.Y));
     }
 
     public override void Update(GameTime gameTime)
@@ -38,6 +41,12 @@ namespace Fleet.Entity
       //didnt use a video tho so im proud
       this.velocity = new Vector2((float)Math.Cos(rotation), (float)Math.Sin(rotation)); // calculates a given unit vector based off of the player rotation we already have
       this.position += this.velocity * 100f; //with a unit vector established we just apply a speed and hurray it works. M A G I C
+
+
+      // code to add - if projectile active for too long set isActive to false so garbage collector can delete it
+      timer--;
+      if (timer < 0)
+        isActive = false;
 		}
 
 		public override void Draw(SpriteBatch spriteBatch, GameTime gameTime)
