a926e3b5c84eef1af16bcf73f8ad9ced78db3282
Stephen Eisen <stephen.eisen27@gmail.com>
2019-01-13 10:10:09 -0500

    Implemented UI Bar Components

--- Fleet/Fleet/Entity/Enemy.cs
+++ Fleet/Fleet/Entity/Enemy.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Fleet.Managers;
+using Fleet.Screen;
 
 namespace Fleet.Entity
 {
@@ -10,8 +11,14 @@ namespace Fleet.Entity
 		private Vector2 _playerPosition;
 		private Vector2 _targetDirection;
 		private float _targetRotation;
+		private BarComponent barComponent;
+		private Vector2 barOffset;
 
-		public Enemy(string filePath) : base(filePath) { }
+		public Enemy(string filePath) : base(filePath)
+		{
+			barComponent = new BarComponent(position, new Vector2(200, 50), 100);
+			barOffset = new Vector2(0, 200);
+		}
 
 		private void TurnToFace(Vector2 location)
 		{
@@ -29,6 +36,8 @@ namespace Fleet.Entity
 			_playerPosition = GameManager.Instance.player.position;
 			TurnToFace(_playerPosition);
 			position = Vector2.Lerp(position, _playerPosition, 0.005f);
+
+			barComponent.Update(100, position + barOffset);
 		}
 
 		public override void Draw(SpriteBatch spriteBatch, GameTime gameTime)
@@ -41,6 +50,8 @@ namespace Fleet.Entity
 			//spriteBatch.Draw(box, position, null, Color.Red, rotation, origin, scale, SpriteEffects.None, 1);
 
 			spriteBatch.Draw(this.Texture, position, null, color, (rotation), origin, scale, SpriteEffects.None, 1);
+
+			barComponent.Draw(spriteBatch);
 		}
 	}
 }
